250511ca612f85102397e2354ea44c58230ed844
MayPereira2023 <143918072+MayPereira2023@users.noreply.github.com>
2023-09-04 16:33:18 -0300

    TrabajoPractico1

---
+++ StaticMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico
+{
+    internal class StaticMain
+    {
+
+        static void Main(string[] args)
+        {
+             Tp1 ejercicio = new Tp1();
+            Console.WriteLine(DateTime.Now.ToString("yyyy'-'MM'-'dd'HORA'HH':'mm':'ss"));
+            Console.ReadLine();
+            Console.WriteLine("Ejercicio 1 Suma de dos variables");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Ingrese el numero A:");
+            int numa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero B");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El resultado de la sume es:" + ejercicio.sumarNumero(numa,numb));
+            Console.ReadLine();
+            Console.WriteLine("Ejercicio 2 Cortar Cadena");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Ingrese una cadena de caracteres de 8 posiciones:");
+            String cadena = (Console.ReadLine());
+            Console.WriteLine("Resultado:" + ejercicio.validarCadena(cadena));
+            Console.WriteLine(" ");
+            Console.WriteLine("Indique una opcion:");
+            Console.WriteLine("1 :BLANCO     2 :AZUL     3: NEGRO");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Resultado: " + ejercicio.mostrarColor(opcion));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Tp1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico
+{
+    internal class Tp1
+    {
+
+        public int sumarNumero(int numa, int numb)
+        {
+            int suma;
+            suma = numa + numb;
+            return suma;
+
+        }
+        public string validarCadena(String cadena)
+        {
+            //tring cadena;
+            //Console.WriteLine("Ejercicio 2 Cortar Cadena");
+            //Console.WriteLine("----------------------------------");
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Ingrese una cadena de caracteres de 8 posiciones:");
+            //cadena = (Console.ReadLine());
+            if (cadena.Length >= 4 && cadena.Length <= 8)
+            {
+                cadena = cadena.Substring(0, 4);
+                //Console.WriteLine(" ");
+                //Console.WriteLine("La cadena cortada es : " + cadena); // Esto imprimirá "cadena"
+                //Console.ReadLine();
+                //DateTime fechaHoraActual = DateTime.Now;
+                //Console.WriteLine("Fecha y Hora Actual: " + fechaHoraActual);
+                return cadena;
+            }
+            else
+            {
+                cadena = "La cadena debe contener entree 4 y menos de 8 carecteres";
+                return cadena;
+
+            }
+            //public string mostrarFecha() {
+
+            //    DateTime fechaHoraActual = DateTime.Now;
+            //    return fechaHoraActual.ToString();
+
+            //}
+        }
+            public string mostrarColor(int opcion) {
+
+                string colores = "SIN ELECCION";
+                            
+                switch (opcion)
+                {
+                    case 1:
+                        colores = "Blanco"
+                        ; break;
+                    case 2:
+                        colores = "Azul"
+                        ; break;
+                    case 3:
+                        colores = "Negro"
+                        ; break;
+                    default:
+                         colores ="Seleccion incorrecta";
+                        break;
+                                       
+                }
+                  return colores;
+            
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    
